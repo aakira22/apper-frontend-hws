c0251075ab08e21762860527a64d75d8265e0f81
Arthur Diwa <arthurakira.diwa@gcash.com>
2022-03-23 19:49:40 +0800

    first commit

---
+++ HW1/HW1-1.js
@@ -0,0 +1,22 @@
+var sampleArray = [5,3,2,10,'a']
+
+function transform(intArray){
+
+  //integer array checker
+  if (!intArray.every(element => typeof(element)=='number')){
+    console.log('Invalid input.')
+    return null
+  } 
+
+  //squaring then sorting
+  else {
+    intArray = intArray.map( x => x ** 2)  
+    newArray = intArray.sort((a, b) => (a - b))
+    return newArray
+  }
+
+}
+
+a = transform(sampleArray)
+
+console.log('Returned Array: '+a)
